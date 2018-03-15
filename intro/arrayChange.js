ccf37c1c09213fd8456226993462be95f8ab71d5
Elis <34626570+SunSince90@users.noreply.github.com>
2018-03-15 16:52:04 +0100

    added arrayChange problem

---
+++ intro/arrayChange.js
@@ -0,0 +1,19 @@
+//--------------------------------------------------
+//  Code Fights: arrayChange
+//--------------------------------------------------
+
+function arrayChange(inputArray) 
+{
+    let moves = 0;
+    
+    for(let i = 1; i < inputArray.length; ++i)
+    {
+        if(inputArray[i] <= inputArray[i-1])
+        {
+            moves += inputArray[i-1] -inputArray[i] +1;
+            inputArray[i] = inputArray[i-1] +1;
+        }
+    }
+    
+    return moves;
+}
