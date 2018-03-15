bc471a873c98b557305ed509b786573ed57e7b55
Elis <34626570+SunSince90@users.noreply.github.com>
2018-03-15 17:20:09 +0100

    added removeKFromList problem

---
+++ interview practice/linked lists/removeKFromList.cpp
@@ -0,0 +1,43 @@
+//--------------------------------------------------
+//  Code Fights: removeKFromList
+//--------------------------------------------------
+
+/*  NOTE: I did not add a main function. This is just the function as provided by code fights. */
+
+// Definition for singly-linked list:
+// template<typename T>
+// struct ListNode {
+//   ListNode(const T &v) : value(v), next(nullptr) {}
+//   T value;
+//   ListNode *next;
+// };
+//
+ListNode<int> * removeKFromList(ListNode<int> * l, int k) 
+{    
+    ListNode<int>* toDelete = nullptr;
+    ListNode<int>* previous = nullptr;
+    ListNode<int>* n = l;
+    
+    while(n != nullptr)
+    {
+        if(n->value == k)
+        {
+            toDelete = n;
+            
+            if(previous != nullptr) previous->next = n->next;                
+            else l = n->next;       
+
+        }
+        else previous = n;
+        
+        n = n->next;
+        
+        if(toDelete != nullptr)
+        {
+            delete toDelete;
+            toDelete = nullptr;
+        }
+    }
+    
+    return l;
+}
