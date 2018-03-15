8c126635a998609fbd5e9cf6788aff02417236e0
Elis <34626570+SunSince90@users.noreply.github.com>
2018-03-15 15:56:17 +0100

    added solution

---
+++ intro/FileNaming.js
@@ -0,0 +1,21 @@
+function fileNaming(names) 
+{    
+    const files = new Set(),
+          n = new Map();
+    
+    for(let i = 0; i < names.length; ++i)
+    {
+        if(files.has(names[i]))
+        {
+            let j = 1;
+            while(files.has(names[i] + "(" + j + ")")) ++j;
+            
+            names[i] += "(" + j + ")";
+            ++j;
+            files.add(names[i]);
+        }
+        else files.add(names[i]);
+    }
+    
+    return names;
+}
